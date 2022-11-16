f01cc9520ad4177c402bb536cb1173467d9d9cf2
thenriet <dabalx@gmail.com>
2022-11-16 17:12:53 +0100

    findAll and findOneBy tested and approved

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,19 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
+use App\Entity\Program;
+
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
+    
+   
+    
     #[Route('/', name: 'index')]
-
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+            'programs' => $programs,
          ]);
     }
-
+    
+    /*
     #[Route('/list/{page}', requirements: ['page'=>'\d+'], name: 'list')]
 
     public function list(int $page = 1): Response
@@ -26,12 +35,16 @@ class ProgramController extends AbstractController
             'page' => $page
         ]);
     }
+    */
 
-    #[Route('/show/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'program_id')]
-    public function show(int $id = 1): Response
+    #[Route('/show/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'show')]
+    public function show(int $id = 1, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
         return $this->render('program/show.html.twig', [
-            'id' => $id
+            'id' => $id,
+            'program' => $program
         ]);
     }
 }
