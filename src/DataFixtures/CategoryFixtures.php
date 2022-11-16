075d11a438bc26f8d39ed6b54f586a80473fa030
thenriet <dabalx@gmail.com>
2022-11-16 10:41:13 +0100

    seed done

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    CONST CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Animation',
+        'Fantastique',
+        'Horreur',
+        'Romance',
+        'Thriller',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        foreach (self::CATEGORIES as $key => $categoryName) 
+        {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
